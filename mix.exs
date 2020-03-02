2caa57f0441377b5679e1d74841cf5eaa6a28698
unknown <daniel.marin.94@gmail.com>
2020-03-03 00:15:09 +0100

    Add missing dependency excoveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Escipion.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -29,7 +30,8 @@ defmodule Escipion.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:httpoison, "~> 1.6"},
       {:logger_file_backend, "~> 0.0.11"},
-      {:credo, "~> 1.2", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.2", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.12"}
     ]
   end
 end
