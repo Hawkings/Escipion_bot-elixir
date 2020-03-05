9670671c26d35da06cbd73ec2281433e175ad6c2
unknown <daniel.marin.94@gmail.com>
2020-03-05 23:38:41 +0100

    Improved unit tests for throttling

--- test/telegram/client/throttling_test.exs
+++ test/telegram/client/throttling_test.exs
@@ -5,10 +5,46 @@ defmodule ThrottlingTest do
   use ExUnit.Case
   alias Escipion.Throttling, as: Throttling
 
-  test "sends message after timeout" do
-    throttler = Throttling.init!(1)
-    :ok = Throttling.throttle(throttler, fn -> IO.puts("test") end)
+  defmodule Counter do
+    use Agent
 
-    :timer.sleep(101)
+    def start_link do
+      Agent.start_link(fn -> 0 end, name: __MODULE__)
+    end
+
+    def get do
+      Agent.get(__MODULE__, & &1)
+    end
+
+    def increment do
+      Agent.update(__MODULE__, & (&1 + 1))
+    end
+  end
+
+  test "counts correctly" do
+    Counter.start_link
+    assert Counter.get == 0
+    Counter.increment
+    assert Counter.get == 1
+    Counter.increment
+    assert Counter.get == 2
+    assert Counter.get == 2
+  end
+
+  test "sends message after timeout" do
+    Counter.start_link
+    throttler = Throttling.init!(100)
+    :ok = Throttling.throttle(throttler, &Counter.increment/0)
+    :ok = Throttling.throttle(throttler, &Counter.increment/0)
+    :ok = Throttling.throttle(throttler, &Counter.increment/0)
+    assert Counter.get == 0
+    :timer.sleep(110)
+    assert Counter.get == 1
+    :timer.sleep(110)
+    assert Counter.get == 2
+    :timer.sleep(110)
+    assert Counter.get == 3
+    :timer.sleep(110)
+    assert Counter.get == 3
   end
 end
